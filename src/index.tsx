c450fa4ff10ab832d3f27be537030faf11ce8be2
Shinohara0319 <yoshinori.shinohara@mvrck.co.jp>
2019-12-02 14:41:32 +0900

    ゲーム勝者判定まで実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,61 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import './index.css';
 
-const Square: React.FC = () => <button className="square">{/* TODO */}</button>;
+interface SquareProps {
+  value: string | null;
+  onClick: () => void;
+}
+
+const Square: React.FC<SquareProps> = ({onClick, value}) => {
+  //const [value, setValue] = React.useState<string | null>(null);
+
+  return <button className="square" onClick={onClick}>{value}</button>
+};
 
 const Board: React.FC = () => {
-  const renderSquare = (i: number) => <Square />;
+  const [squares, setSquares] = React.useState<(string | null)[]>(Array(9).fill(null));
 
-  const status = 'Next player: X';
+  const [xIsNext, setXIsNext] = React.useState(true);
+
+  const renderSquare = (i: number) => <Square
+    value={squares[i]}
+    onClick={() => handleClick(i)}
+  />;
+
+  const handleClick = (i:number) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(nextSquares);
+    setXIsNext(prev => !prev);
+  }
+  const calculateWinner = (squares: (string | null)[]) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? 'X' : 'O'}`;
 
   return (
     <div>
