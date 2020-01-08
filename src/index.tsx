54714cacf6e3cee8ac2665d1606eff2a1435c9c6
Shinohara0319 <yoshinori.shinohara@mvrck.co.jp>
2020-01-08 14:22:34 +0900

    リファクタとprettierの適用

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,45 @@
-import * as React from 'react';
-import ReactDOM, { render } from 'react-dom';
-import './index.css';
+import * as React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+type Squares = (string | null)[];
+type History = { squares: Squares }[];
 
 interface SquareProps {
   value: string | null;
   onClick: () => void;
 }
 
-const Square: React.FC<SquareProps> = ({onClick, value}) => {
-  return <button className="square" onClick={onClick}>{value}</button>
+const Square: React.FC<SquareProps> = ({ value, onClick }) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
 };
 
 interface BoardProps {
-  value: { squares: (string | null)[]};
-  onClick: (squares:(string | null)[]) => void;
+  value: { squares: Squares };
+  onClick: (squares: Squares) => void;
 }
 
-//var test:(a: number, b: number) => number = (a: number, b:number):number => a + b;
+const Board: React.FC<BoardProps> = ({ value, onClick }) => {
+  const xIsNext = !(value.squares.filter(square => square !== null).length % 2);
 
-const Board: React.FC<BoardProps> = ({value, onClick}) => {
-//const Board: React.FC = () => {
-  //const [squares, setSquares] = React.useState<(string | null)[]>(Array(9).fill(null));
+  const renderSquare = (i: number) => (
+    <Square value={value.squares[i]} onClick={() => handleClick(i)} />
+  );
 
-  const [xIsNext, setXIsNext] = React.useState(true);
-
-  const renderSquare = (i: number) => <Square
-    value={value.squares[i]}
-    onClick={() => handleClick(i)}
-  />;
-
-  const handleClick = (i:number) => {
+  const handleClick = (i: number) => {
     if (calculateWinner(value.squares) || value.squares[i]) {
       return;
     }
-
     const nextSquares = value.squares.slice();
-    nextSquares[i] = xIsNext ? 'X' : 'O';
+    nextSquares[i] = xIsNext ? "X" : "O";
     onClick(nextSquares);
-    setXIsNext(prev => !prev);
-  }
-  const calculateWinner = (squares: (string | null)[]) => {
+  };
+
+  const calculateWinner = (squares: Squares) => {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -48,20 +48,24 @@ const Board: React.FC<BoardProps> = ({value, onClick}) => {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6],
+      [2, 4, 6]
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
         return squares[a];
       }
     }
     return null;
-  }
+  };
   const winner = calculateWinner(value.squares);
   const status = winner
     ? `Winner: ${winner}`
-    : `Next player: ${xIsNext ? 'X' : 'O'}`;
+    : `Next player: ${xIsNext ? "X" : "O"}`;
 
   return (
     <div>
@@ -86,21 +90,25 @@ const Board: React.FC<BoardProps> = ({value, onClick}) => {
 };
 
 const Game: React.FC = () => {
-  const [history, setHistory] = React.useState< { squares: (string | null)[]}[] >([{squares : Array(9).fill(null)}]);
-  //<squaresの中身はstring or nullの配列です。配列を配列の中に入れます。>初期値は配列の中にオブジェクトのsquaresを入れています。
-  const handleClick = (squares:(string | null)[]) => {
-    setHistory(history.concat([{squares}]))
-  }
+  const [history, setHistory] = React.useState<History>([
+    { squares: Array(9).fill(null) }
+  ]);
+  const handleClick = (squares: Squares) => {
+    setHistory(history.concat([{ squares }]));
+  };
 
-  const buttons = history.map((unk: any, index: number):React.ReactElement => {
-    const label = index ? 'Go to move #' + index : 'Go to game start';
-    return <li key={index}>
-      <button onClick={() => {
-        setHistory(history.slice(0, index + 1))
-      }}>{label}</button>
-    </li>
-  });
-
+  const buttons = history.map(
+    (_: any, index: number): React.ReactElement => {
+      const label = index ? "Go to move #" + index : "Go to game start";
+      return (
+        <li key={index}>
+          <button onClick={() => setHistory(history.slice(0, index + 1))}>
+            {label}
+          </button>
+        </li>
+      );
+    }
+  );
 
   return (
     <div className="game">
@@ -108,15 +116,10 @@ const Game: React.FC = () => {
         <Board value={history[history.length - 1]} onClick={handleClick} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>
-          {buttons}
-        </ol>
+        <ol>{buttons}</ol>
       </div>
     </div>
-  )
+  );
 };
 
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById("root"));
